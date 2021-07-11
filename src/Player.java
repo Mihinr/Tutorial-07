9f09cedaecc202e6c77bf33f60768a02a69b56ac
asiniweer <85722602+asiniweer@users.noreply.github.com>
2021-07-11 23:11:23 +0530

    update

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,3 @@
-import javax.naming.Name;
-
 public class Player {
     public String Name;
     public int age;
@@ -9,7 +7,9 @@ public class Player {
     public double runRate;
     public double strikeRate;
 
-    public Player (String name,int age, String type, int runsScored,int wickets, double runRate, double strikeRate){
+    public Player(){}
+
+    public Player(String name, int age, String type, int runsScored, int wickets, double runRate, double strikeRate) {
         Name = name;
         this.age = age;
         this.type = type;
@@ -18,37 +18,61 @@ public class Player {
         this.runRate = runRate;
         this.strikeRate = strikeRate;
     }
-    public String getName(){
+
+    public String getName() {
         return Name;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         Name = name;
     }
-    public int getAge(){
+
+    public int getAge() {
         return age;
     }
-    public void setAge(int Age){
+
+    public void setAge(int age) {
         this.age = age;
     }
-    public String getType(){
+
+    public String getType() {
         return type;
     }
-    public void setType(String type){
+
+    public void setType(String type) {
         this.type = type;
     }
-    public int getRunsScored(){
+
+    public int getRunsScored() {
         return runsScored;
     }
-    public void setRunScored(int runsScored){
+
+    public void setRunsScored(int runsScored) {
         this.runsScored = runsScored;
     }
-    public int getWickets(){
+
+    public int getWickets() {
         return wickets;
     }
-    public void setWickets(int wickets){
+
+    public void setWickets(int wickets) {
         this.wickets = wickets;
     }
 
-    public double g
+    public double getRunRate() {
+        return runRate;
+    }
+
+    public void setRunRate(double runRate) {
+        this.runRate = runRate;
+    }
+
+    public double getStrikeRate() {
+        return strikeRate;
+    }
+
+    public void setStrikeRate(double strikeRate) {
+        this.strikeRate = strikeRate;
     }
 }
+
